15f10f112dba58c591503532d6c573dcf1561612
Lucius <luciushehe@protonmail.com>
2023-02-08 13:32:34 -0600

    Added helper functions

---
+++ pkg/helpers/helpers.go
@@ -0,0 +1,56 @@
+package helpers
+
+import (
+	"os"
+	"os/exec"
+	"path"
+)
+
+func CreateDir(baseDir string, name string, initGit bool) (err error) {
+	path := path.Join(baseDir, name)
+	err = os.MkdirAll(path, os.ModePerm)
+
+	if err != nil {
+		return
+	}
+
+	if !initGit {
+		return
+	}
+
+	wd, _ := os.Getwd()
+	defer os.Chdir(wd)
+	os.Chdir(path)
+
+	err = exec.Command("git", "init").Run()
+	return
+}
+
+func AddFiles(baseDir string,
+							dirName string,
+							commit bool,
+							filesNames ...string) (err error) {
+	dir := path.Join(baseDir, dirName)
+	for _, file := range filesNames {
+		data := []byte("File data for: " + file)
+		err = os.WriteFile(dir, data, os.ModePerm)
+		if err != nil {
+			return
+		}
+	}
+
+	if !commit {
+		return
+	}
+
+	wd, _ := os.Getwd()
+	defer os.Chdir(wd)
+
+	os.Chdir(dir)
+	err = exec.Command("git", "add", "-A").Run()
+	if err != nil {
+		return
+	}
+	err = exec.Command("git", "commit", "-m", "Add files").Run()
+	return
+}
