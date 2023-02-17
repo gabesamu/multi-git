1c176741f8c953dbcc5a5220527a0b00a833ea7a
Lucius <luciushehe@protonmail.com>
2023-02-17 13:17:01 -0600

    e2e flag tests

--- e2e/multi_git_test.go
+++ e2e/multi_git_test.go
@@ -4,9 +4,160 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	"github.com/0xlucius/multi-git/e2e"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/0xlucius/multi-git/pkg/helpers"
 )
 
 var _ = Describe("MultiGit", func() {
+	baseDir, _ := filepath.Abs("tmp/test-multi-git")
+	var repoList string
+	var err error
+
+
+	BeforeEach(func ()  {
+		err := helpers.CreateDir(baseDir, "", false)
+		Expect(err).To(BeNil())
+	})
+
+	AfterEach(func ()  {
+		err := os.RemoveAll(baseDir)
+		Expect(err).To(BeNil())
+	})
+
+	Context("When ran with empty/undefined evironment", func ()  {
+		It("Should fail with empty base directory", func ()  {
+			out, err := helpers.RunMultiGit("status", false, "/does not exist", repoList , false)
+			Expect(err).ToNot(BeNil())
+			errMessage := "base dir: '/no-such-dir/' doesn't exist\n"
+			Expect(out).To(HaveSuffix(errMessage))
+		})
+
+		It("Should fail with empty repoList", func ()  {
+			out, err := helpers.RunMultiGit("status", false, baseDir, repoList , false)
+			Expect(err).ToNot(BeNil())
+			Expect(out).To(ContainSubstring("repo list can't be empty"))
+		})
+	})
+
+	Describe("Running Git commands", func ()  {
+		Context("Given proper input", func() {
+			It("Should do git init successfully", func() {
+				err = helpers.CreateDir(baseDir, "dir-1", false)
+				Expect(err).To(BeNil())
+				err = helpers.CreateDir(baseDir, "dir-2", false)
+				Expect(err).To(BeNil())
+				repoList = "dir-1,dir-2"
+
+				output, err := helpers.RunMultiGit("init", false, baseDir, repoList, true)
+				Expect(err).To(BeNil())
+				count := strings.Count(output, "Initialized empty Git repository")
+				Expect(count).To(Equal(2))
+			})
+
+			It("Should do git status successfully for git directories", func() {
+				err = helpers.CreateDir(baseDir, "dir-1", true)
+				Expect(err).To(BeNil())
+				err = helpers.CreateDir(baseDir, "dir-2", true)
+				Expect(err).To(BeNil())
+				repoList = "dir-1,dir-2"
+
+				output, err := helpers.RunMultiGit("status", false, baseDir, repoList, true)
+				Expect(err).To(BeNil())
+				count := strings.Count(output, "nothing to commit")
+				Expect(count).To(Equal(2))
+
+				output, err = helpers.RunMultiGit("status", false, baseDir, repoList, false)
+				Expect(err).To(BeNil())
+				count = strings.Count(output, "nothing to commit")
+				Expect(count).To(Equal(2))
+
+			})
+
+			It("Should create branches successfully", func() {
+				err = helpers.CreateDir(baseDir, "dir-1", true)
+				Expect(err).To(BeNil())
+				err = helpers.CreateDir(baseDir, "dir-2", true)
+				Expect(err).To(BeNil())
+				repoList = "dir-1,dir-2"
+
+				output, err := helpers.RunMultiGit("checkout -b test-branch", false, baseDir, repoList, true)
+				Expect(err).To(BeNil())
+				count := strings.Count(output, "Switched to a new branch 'test-branch'")
+				Expect(count).To(Equal(2))
+
+				output, err = helpers.RunMultiGit("checkout -b test-branch", false, baseDir, repoList, false)
+				Expect(err).To(BeNil())
+				count = strings.Count(output, "Switched to a new branch 'test-branch'")
+				Expect(count).To(Equal(2))
+			})
+		})
+
+		Context("When ran on non-git repo", func() {
+			It("Should fail git status", func() {
+				err = helpers.CreateDir(baseDir, "dir-1", false)
+				Expect(err).To(BeNil())
+				err = helpers.CreateDir(baseDir, "dir-2", false)
+				Expect(err).To(BeNil())
+				repoList = "dir-1,dir-2"
+
+				output, err := helpers.RunMultiGit("status", false, baseDir, repoList, true)
+				Expect(err).To(BeNil())
+				Expect(output).To(ContainSubstring("fatal: not a git repository"))
+
+				output, err = helpers.RunMultiGit("status", false, baseDir, repoList, false)
+				Expect(err).To(BeNil())
+				Expect(output).To(ContainSubstring("fatal: not a git repository"))
+
+			})
+		})
+	})
+
+	Describe("ignoreErrors flag", func ()  {
+		Context("When a directory is invalid", func ()  {
+			Context("ignoreErrors is true", func ()  {
+				It("Should succeed for all valid directories", func ()  {
+					err = helpers.CreateDir(baseDir, "dir-1", false)
+					Expect(err).To(BeNil())
+					err = helpers.CreateDir(baseDir, "dir-2", true)
+					Expect(err).To(BeNil())
+					repoList = "dir-1,dir-2"
+
+					output, err := helpers.RunMultiGit("status", true, baseDir, repoList, true)
+					Expect(err).To(BeNil())
+					Expect(output).To(ContainSubstring("[dir-1]: git status\nfatal: not a git repository"))
+					Expect(output).To(ContainSubstring("[dir-2]: git status\nOn branch master"))
+
+					output, err = helpers.RunMultiGit("status", true, baseDir, repoList, false)
+					Expect(err).To(BeNil())
+					Expect(output).To(ContainSubstring("[dir-1]: git status\nfatal: not a git repository"))
+					Expect(output).To(ContainSubstring("[dir-2]: git status\nOn branch master"))
+				})
+			})
+
+			Context("ignoreErrors is false", func ()  {
+				It("Should fail on first invalid directory", func ()  {
+					err = helpers.CreateDir(baseDir, "dir-1", false)
+					Expect(err).To(BeNil())
+					err = helpers.CreateDir(baseDir, "dir-2", true)
+					Expect(err).To(BeNil())
+					repoList = "dir-1,dir-2"
+
+					output, err := helpers.RunMultiGit("status", false, baseDir, repoList, true)
+					Expect(err).To(BeNil())
+					Expect(output).To(ContainSubstring("[dir-1]: git status\nfatal: not a git repository"))
+					Expect(output).ShouldNot(ContainSubstring("[dir-2]"))
+
+					output, err = helpers.RunMultiGit("status", false, baseDir, repoList, false)
+					Expect(err).To(BeNil())
+					Expect(output).To(ContainSubstring("[dir-1]: git status\nfatal: not a git repository"))
+					Expect(output).ToNot(ContainSubstring("[dir-2]"))
+				})
+			})
+		})
+	})
+
 
 })
